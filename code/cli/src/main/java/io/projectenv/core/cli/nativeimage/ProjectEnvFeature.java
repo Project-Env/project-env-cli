b5b8b9f692a9da93d65c6f565bbac6a0a0bf2d9a
repolevedavaj <jonas.bamberger@gmail.com>
2022-12-25 14:57:06 +0100

    Fix native image build

--- code/cli/src/main/java/io/projectenv/core/cli/nativeimage/ProjectEnvFeature.java
+++ code/cli/src/main/java/io/projectenv/core/cli/nativeimage/ProjectEnvFeature.java
@@ -2,17 +2,13 @@ package io.projectenv.core.cli.nativeimage;
 
 import com.google.gson.TypeAdapterFactory;
 import com.google.gson.annotations.SerializedName;
-import com.oracle.svm.core.annotate.AutomaticFeature;
-import io.projectenv.core.commons.nativeimage.NativeImageHelper;
 import io.projectenv.core.commons.process.ProcessOutput;
 import io.projectenv.core.toolsupport.spi.ToolSupport;
 import org.apache.commons.compress.archivers.zip.ZipExtraField;
 import org.graalvm.nativeimage.hosted.Feature;
-import org.graalvm.nativeimage.hosted.RuntimeClassInitialization;
 
-import java.io.IOException;
+import static io.projectenv.core.commons.nativeimage.NativeImageHelper.*;
 
-@AutomaticFeature
 public class ProjectEnvFeature implements Feature {
 
     private static final String BASE_PACKAGE = "io.projectenv";
@@ -26,30 +22,22 @@ public class ProjectEnvFeature implements Feature {
     }
 
     private void configureProcessOutputWriter() {
-        RuntimeClassInitialization.initializeAtBuildTime(ProcessOutput.class);
+        initializeAtBuildTime(ProcessOutput.class);
     }
 
     private void registerZipExtraFieldClasses() {
         // Since Apache Commons Compress uses reflection to register the ZipExtraField
         // implementations, we have to register them for Reflection support.
-        NativeImageHelper.registerClassAndSubclassesForReflection(ZipExtraField.class);
+        registerClassAndSubclassesForReflection(ZipExtraField.class);
     }
 
     private void registerToolSupportService() {
-        try {
-            NativeImageHelper.registerService(ToolSupport.class);
-        } catch (IOException e) {
-            throw new IllegalStateException("failed to register services for usage in native-image");
-        }
+        registerService(ToolSupport.class);
     }
 
     private void registerGsonSupport() {
-        try {
-            NativeImageHelper.registerService(TypeAdapterFactory.class);
-            NativeImageHelper.registerFieldsWithAnnotationForReflection(BASE_PACKAGE, SerializedName.class);
-        } catch (IOException e) {
-            throw new IllegalStateException("failed to register services for usage in native-image");
-        }
+        registerService(TypeAdapterFactory.class);
+        registerFieldsWithAnnotationForReflection(BASE_PACKAGE, SerializedName.class);
     }
 
 }
